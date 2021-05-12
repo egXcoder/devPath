15d619747f5bc2b498b95dd9f51c10f9dd5822c6
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-05-12 15:40:11 +0200

    indent homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Category;
+
 class HomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $selectedCategory = Category::findOrFail(1);
         $categories = Category::all();
         return view('index', ['categories' => $categories, 'selectedCategory' => $selectedCategory]);
     }
 
-    public function showSpecificCategory(Category $category) {
-        $categories = Category::all();
-        return view('index', ['categories' => $categories, 'selectedCategory' => $category]);
+    public function showSpecificCategory(Category $category)
+    {
+        return view('index', ['categories' => Category::all(), 'selectedCategory' => $category]);
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -40717,8 +40717,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\devPath\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\devPath\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp749\htdocs\devPath\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp749\htdocs\devPath\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ }),
